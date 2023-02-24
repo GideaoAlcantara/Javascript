7194394e0756016d6b76ba7afab6242fd0894a45
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-02-24 15:23:28 -0300

    ok

---
+++ desafio/desafio04.2/des03/pensao.js
@@ -0,0 +1,20 @@
+
+function pensao() {
+    var name = document.getElementById('nome')
+    var sex = document.getElementsByName('tsex')
+    var idd = document.getElementById('idade')
+    var trab = document.querySelector('input#tb')
+    var uniao = document.querySelector('input#uniao')
+
+    var resp = 
+
+    var idade = Number(idd.value)
+    var tempo = Number(trab.value)
+    var junto = Number(uniao.value)
+
+    if(sex[0].checked && idade<22 && junto<2){
+        
+    }else{
+     
+    }
+}
