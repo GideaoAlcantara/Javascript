8e514f9240be5267128b6e82cbb37363007ace61
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-03-01 19:52:01 -0300

    ok

--- desafio/repetição/pensão/script/pensao.js
+++ desafio/repetição/pensão/script/pensao.js
@@ -1,10 +1,48 @@
 
-function pensao(){
+function pensao() {
+   let nome = document.getElementById('nomi')
+   let idade = document.querySelector('input#ydade')
+   let trab = document.querySelector('input#ttrab')
+   let uniao = document.querySelector('input#tuniao')
+   let respo = document.getElementById('resp')
 
-   var nome = window.document.querySelector('input#ydade')
-   var resposta = document.querySelector('p.resp')
+   if(nome.value == 0 || idade.value == 0 || trab.value == 0 || uniao.value == 0){
+      window.alert('[ERRO], preencha o formulario !')
+   }else{
+      let nmm = String(nome.value)
+      let idd = Number(idade.value)
+      let tbb = Number(trab.value)
+      let unn = Number(uniao.value)
 
-   if(nome <= 20){
-      resposta.innerHTML = ` deu certo `
+      if(unn >= 2 && tbb >= 18 && idd < 22){
+         respo.innerHTML = ` Olá <strong>${nmm}</strong>, seu tempo de pensão é de <strong>6 anos</strong>`
+      }else{
+          if(unn >= 2 && tbb >= 18 && idd >= 22 && idd < 27){
+            respo.innerHTML = ` Olá <strong>${nmm}</strong>, seu tempo de pensão é de <strong>8 anos</strong>`
+          }else{
+            if(unn >= 2 && tbb >= 18 && idd >= 27 && idd <= 30){
+               respo.innerHTML = ` Olá <strong>${nmm}</strong>, seu tempo de pensão é de <strong>10 anos</strong>`
+            }else{
+               if(unn >= 2 && tbb >= 18 && idd >= 31 && idd <= 41){
+                  respo.innerHTML = ` Olá <strong>${nmm}</strong>, seu tempo de pensão é de <strong>15 anos</strong>`
+               }else{
+                  if(unn >= 2 && tbb >= 18 && idd >= 42 && idd <= 44){
+                     respo.innerHTML = ` Olá <strong>${nmm}</strong>, seu tempo de pensão é de <strong>20 anos</strong>`
+                  }else{
+                     if(unn >= 2 && tbb >= 18 && idd >= 45){
+                        respo.innerHTML = ` Olá <strong>${nmm}</strong>, seu tempo de pensão é de <strong>Vitalicio</strong>`
+                     }else{
+                        respo.innerHTML = ` Olá <strong>${nmm}</strong>, seu tempo de pensão é de <strong>4 meses</strong>`
+                     }
+                  }
+               }
+            }
+          }
+      }
    }
-  }
+}
+
+
+
+if(unn >= 2 && tbb >= 18 && idd < 22)
+respo.innerHTML = ` Olá <strong>${nmm}</strong>, seu tempo de pensão é de <strong>6 anos</strong>`
