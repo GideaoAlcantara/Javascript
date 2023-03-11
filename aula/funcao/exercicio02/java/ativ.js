ae7e4048d26ee7866e5f4dd9b7943b3c9b7322b5
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-03-11 17:35:17 -0300

    aula concluida

---
+++ aula/funcao/exercicio02/java/ativ.js
@@ -0,0 +1,77 @@
+// 01 VARIAVEIS
+
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+
+let valores = [] // sevir para analisar os dados
+
+
+// 02 FUNÇÃO PRINCIPAL
+
+function adicionar() {
+
+    //isNumero e inLista são funcões com parametro das varieveis NUM e Valores
+    
+        if (isNumero(num.value) && !inLista(num.value,valores)) {
+            
+        valores.push(Number(num.value))
+
+        let item = document.createElement('option')
+
+        item.text = `valor ${num.value} adicionado`
+
+        lista.appendChild(item)
+
+        }else{
+            window.alert('Valor invalido ou já encontrado na lista.')
+        }
+    }
+
+    //limpatela
+
+num.value = ''
+num.focus()
+
+
+// 03 FUNÇAO SECUNDARIA
+
+function isNumero(n) {
+
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+    
+}
+
+function inLista(n,l) {
+                            // -1 é  o valor que não foi encontrado na lista
+
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+    
+}
+
+//PRINCIPAL PARTE II
+
+function finalizar() {
+    
+    if(valores.length == 0){
+           window.alert(`adicione valores antes de finalizar`)
+    }else{
+        let tot = valores.length
+
+        res.innerHTML = ''
+
+        res.innerHTML += `<p>Ao todo, temos ${tot} numeros cadastrados</p>`
+    }
+
+}
+
+
+
