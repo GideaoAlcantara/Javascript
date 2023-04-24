591ea1904344e3ef89ea8a1860022fead4552437
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-04-24 09:37:46 -0300

    AULA JAVA

--- revisao/ex4/ex4.js
+++ revisao/ex4/ex4.js
@@ -1,7 +1,45 @@
-let abc = 12
+/*function contar() {
+  let  numb01 = window.document.getElementById('inumb')
+  var  numb02 = window.document.querySelector('input#iinumb')
+  let  resppp = window.document.getElementsByClassName('resp')[0]
 
-if (abc >= 12) {
-    console.log('maior que doze');
+  let num1 = Number(numb01.value)
+  let num2 = Number(numb02.value)
+
+  if (numb01.value == 0 && numb02.value == 0) {
+        console.log(`[ERRO],Valores vazios.`);
+  } else {
+    if (num2 >= num1) {
+            do {
+                resppp.innerHTML += `${num1} `
+                num1++
+            } while (num2 >= num1);
+    } else {
+        if(num2 <= num1){
+            do {
+                resppp.innerHTML += `${num1} `
+                num1--
+            } while (num2 <= num1); 
+        }
+    }
+  }
+}
+*/
+
+let X = [ 4, 2,7 , 3, 0, 6,]
+
+X[6] = 10
+
+X.push(12)
+
+X.unshift(-1)
+
+
+
+for (let A = 0; A < X.length; A++) {
+  
+  console.log(`${X[A]}`);
+  
 }
 
 
