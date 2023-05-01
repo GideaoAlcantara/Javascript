09f5bdb88b9083a43536595876cdff67ef068ba6
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-05-01 09:34:15 -0300

    ok

--- revisao/dia03/rev03.js
+++ revisao/dia03/rev03.js
@@ -1,9 +1,59 @@
-let exemplo = []
+/*let exemplo = [9,2,4,7,1,0,3,5]
 
-exemplo[0] = 'presente'
+let pp = 1
 
-exemplo.push('futuro')
+let pos = exemplo.indexOf(pp)
 
-exemplo.unshift('passado')
+console.log();1 
 
-console.log(exemplo[0],exemplo[1],exemplo[2]);
+if (pos == -1) {
+    console.log(`[ERRO] Valor não encontrado !`);
+} else {
+    console.log(`a casa ${pos} tem o valor ${pp}`);
+}
+
+console.log();
+
+for (let valor in exemplo) {
+    if (exemplo.hasOwnProperty.call(exemplo, valor)) {
+        let element = exemplo[valor];
+        console.log(`${element}`);
+    }
+}
+
+console.log();
+
+exemplo.forEach(valor02 => {
+    console.log(`${exemplo[valor02]}`);
+});
+
+console.log();
+
+for (const iterator of exemplo) {
+    console.log(exemplo[iterator]);
+}
+
+/*for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+*/
+
+let valor = []
+
+valor[0] = 'presente'
+
+valor.push('futuro')
+
+valor.unshift('passado')
+
+valor.shift()
+
+
+
+console.log(valor[0],valor[1],valor[2]);
+
+
+
