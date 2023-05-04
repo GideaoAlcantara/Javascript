f37371cf990f5b519f1961fc9904f5bb0d20ae71
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-05-04 18:51:00 -0300

    ok

--- revisao/dia02/dia02.js
+++ revisao/dia02/dia02.js
@@ -1,6 +1,41 @@
-let y = [3,7,0,1,4,8,9,2,5,6]
+ function contagem() {
+   
+    let iinicio = window.document.querySelector('input#inicio')
+    let ifim = window.document.getElementById('fim')
+    let pazzo = window.document.querySelector('input#passo')
 
-for (let x = 0; x < y.length ; x++) {
-    
-    console.log(` o numero ${y[x]} estar na casa ${x}`);
-}
+    var respp = window.document.getElementById('resp')
+
+    var inicio = Number(iinicio.value)
+
+    var fim = Number(ifim.value)
+
+    var passo = Number(pazzo.value)
+
+    if (inicio == 0 && fim == 0 && passo == 0) {
+       
+          window.alert('[ERRO] valores vazio !')
+
+    } else {
+      
+       if (inicio >= fim) {
+        
+             while (inicio >= fim) {
+                respp.innerHTML += `${inicio} - `
+                inicio = inicio - passo
+             }
+
+       } else {
+             if (inicio <= fim) {
+                
+                while (inicio <= fim) {
+                    respp.innerHTML += `${inicio} - `
+                    inicio = inicio + passo
+                }
+
+             }
+       }
+
+    }
+ }
+
---
+++ revisao/dia02/node_b091383e03410.tmp.js
@@ -0,0 +1,16 @@
+/*let y = [3,7,0,1,4,8,9,2,5,6]
+
+for (let x = 0; x < y.length ; x++) {
+    
+    console.log(` o numero ${y[x]} estar na casa ${x}`);
+}*/
+
+let a = 0
+let b = 10
+
+while (b >= a ) {
+    do {
+       console.log(a); 
+       a++
+    } while (a != b);
+}
