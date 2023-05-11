12cd64685f612c0aef5f823aa1c09ec2918a989d
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-05-11 17:58:01 -0300

    ok

--- revisao/dia02/dia02.js
+++ revisao/dia02/dia02.js
@@ -6,11 +6,26 @@
 
     var respp = window.document.getElementById('resp')
 
+    let head = window.document.getElementsByTagName('body')[0]
+
     var inicio = Number(iinicio.value)
 
     var fim = Number(ifim.value)
 
     var passo = Number(pazzo.value)
+
+
+    let imagem = document.createElement('img')
+
+    imagem.setAttribute('src' , 'https://meusport.com/wp-content/uploads/2023/05/52868601732_8af7e96199_k-392x272.jpg')
+
+    head.appendChild(imagem)
+
+
+
+
+
+
 
     if (inicio == 0 && fim == 0 && passo == 0) {
        
