c71a6987881c1dd12ccad8abee2bc5dc180e8274
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-07-05 10:56:20 -0300

    ok

--- revisao/ex1/ex001.js
+++ revisao/ex1/ex001.js
@@ -3,16 +3,24 @@ function contar() {
     let num01 = window.document.getElementById('inum')
     let num02 = window.document.querySelector('input#iinum')
     let exit = window.document.getElementById('iresp')
+    let caixa = window.document.getElementById('caixa12')
 
     let valor01 = Number(num01.value)
     let valor02 = Number(num02.value)
+
+    let img = window.document.createElement('img')
+    img.setAttribute('src','https://cdn.pixabay.com/photo/2018/05/16/01/45/clouds-3404728_1280.jpg')
+    img.setAttribute('width','300px')
+    caixa.appendChild(img)
+     
+
 
    for (let index = 0; index < valor02; index++) {
   
     
         exit.innerHTML+=`${valor01} `
         valor01+=1
-       
+
    
 
    }
@@ -60,8 +68,36 @@ if (valor02 > valor01) {
 
 /*aritmético*/
 
-let z = [1,2,3,4,5,6,7,8,9,10]
 
-for(let x of z){
-      console.log(x);
+/*function acao(...numero){
+  
+   for (const teste of numero) {
+      console.log(teste);
+   }
 }
+
+console.log(acao(2,3,5,6,7,8,9,0));
+
+let anonima =(n1,n2)=>{
+    return n1+n2
+}
+console.log(anonima(3,9));*/
+
+
+
+const soma = (...valores) =>{
+    
+    const somar = val =>{
+
+        let res = 0
+
+        for (let V of val) {
+            res += V
+        }
+        return res
+    }
+
+    return somar(valores)
+}
+
+console.log(soma(2,4,6));
